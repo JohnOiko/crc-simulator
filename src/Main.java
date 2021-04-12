fbb7b2434b41c86ae077af2fa6728eabfcd4765d
John Oiko <ioannisoa@csd.auth.gr>
2021-04-12 11:12:40 +0300

    Changed the way input is received from the command line.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,65 @@
 import java.util.Scanner;
-import java.lang.Math;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String input;
+        int numberOfBits = 1;
         System.out.println("Give desired number of the message's bits (k), up to 31 bits are supported:");
-        int numberOfBits = Integer.parseInt(in.nextLine());
+        boolean flag = true;
+        while (flag) {
+            input = in.nextLine();
+            if (isInteger(input)) {
+                if (Integer.parseInt(input) > 1 && Integer.parseInt(input) < 32 ) {
+                    flag = false;
+                    numberOfBits = Integer.parseInt(input);
+                }
+            }
+            if (flag)
+                System.out.println("Please give a valid number of bits (k) that is between 2 and 31:");
+        }
+
+        BinaryNumber P = new BinaryNumber(0);
         System.out.println("\nGive binary number to divide the messages with (P):");
-        BinaryNumber P = new BinaryNumber(in.nextLine());
+        flag = true;
+        while (flag) {
+            input = in.nextLine();
+            if (isBinary(input)) {
+                if (input.length() > 0 && input.length() < numberOfBits) {
+                    flag = false;
+                    P.setBinaryValue(input);
+                }
+            }
+            if (flag)
+                System.out.println("Please give a valid binary number to divide the messages with (P) that has less than " + numberOfBits + " digits:");
+        }
+
+        double errorRate = 0;
         System.out.println("\nGive bit error rate (E) as a decimal:");
-        double errorRate = Double.parseDouble(in.nextLine());
+        flag = true;
+        while (flag) {
+            input = in.nextLine();
+            if (isDecimalPercentage(input)) {
+                    flag = false;
+                    errorRate = Double.parseDouble(input);
+            }
+            if (flag)
+                System.out.println("Please give a valid bit error rate (E) as a decimal that is between 0 and 1:");
+        }
+
+        int sampleCount = 1;
         System.out.println("\nGive amount of samples to use:");
-        int sampleCount = Integer.parseInt(in.nextLine());
+        flag = true;
+        while (flag) {
+            input = in.nextLine();
+            if (isInteger(input)) {
+                flag = false;
+                sampleCount = Integer.parseInt(input);
+            }
+            if (flag)
+                System.out.println("Please give a valid amount of samples to use:");
+        }
 
         int changedCount = 0;
         int caughtChangedCount = 0;
@@ -52,5 +99,35 @@ public class Main {
         System.out.printf("%nPercent of changed during transmission: %.2f%%%n", ((double)changedCount/sampleCount)*100);
         System.out.printf("Percent of changed during transmission that were caught: %.2f%%%n", ((double)caughtChangedCount/sampleCount)*100);
         System.out.printf("Percent of changed during transmission that were not caught: %.2f%%%n", ((double)notCaughtChangedCount/sampleCount)*100);
+    }
+
+    public static boolean isInteger(String string) {
+        for (int i = 0 ; i < string.length() ; i++) {
+            if (!(Character.isDigit(string.charAt(i))))
+                return false;
+        }
+        return true;
+    }
+
+    public static boolean isBinary(String string) {
+        for (int i = 0 ; i < string.length() ; i++) {
+            if (!(string.charAt(i) == '0' || string.charAt(i) == '1'))
+                return false;
+        }
+        return true;
+    }
+
+    public static boolean isDecimalPercentage(String string) {
+        if (string.length() <= 1) {
+            return string.equals("1") || string.equals("0");
+        }
+        if (string.charAt(0) != '0' || string.charAt(1) != '.') {
+            return string.equals("1.0") || string.equals("1.00") || string.equals("0.0") || string.equals("0.00");
+        }
+        for (int i = 2 ; i < string.length() ; i++) {
+            if (!(Character.isDigit(string.charAt(i))))
+                return false;
+        }
+        return true;
     }
 }

128abc87e7f830f49445de82fca969ec75e885b5
John Oiko <ioannisoa@csd.auth.gr>
2021-05-27 20:01:44 +0300

    Added comments to the first half of the class Main.java and changed the chance of setting the original message's bits to 1.

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,24 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.Scanner;
 
+// Main class that is to be run.
 public class Main {
 
+    // Main function.
     public static void main(String[] args) {
+        // Scanner to read input from the user.
         Scanner in = new Scanner(System.in);
         String input;
+        // Default number of bits is 1.
         int numberOfBits = 1;
         System.out.println("Give desired number of the message's bits (k), up to 29 bits are supported:");
+        // Flag that is used to stop the while loop that reads input from the user.
         boolean flag = true;
+        // While loop that stops when the input is valid by making the flag false.
         while (flag) {
+            // Read the next line.
             input = in.nextLine();
+            // If the input is an integer and has 1-4 digits and has a value between 2 and 29, make the flag false to break the loop and save the number of bits.
             if (isInteger(input)) {
                 if (input.length() > 0 && input.length() < 5) {
                     if (Integer.parseInt(input) > 1 && Integer.parseInt(input) < 30 ) {
@@ -20,56 +28,77 @@ public class Main {
                     }
                 }
             }
+            // If the flag is still true that means the given input was not valid and a message requesting input must be shown again.
             if (flag)
                 System.out.println("Please give a valid number of bits (k) that is between 2 and 29:");
         }
 
+        // Default P is 0.
         BinaryNumber P = new BinaryNumber(0);
         System.out.println("\nGive binary number to divide the messages with (P):");
+        // Reset the flag to true.
         flag = true;
+        // While loop that stops when the input is valid by making the flag false.
         while (flag) {
             input = in.nextLine();
+            // If the input is a binary number, has between 2 to 32 - numberOfBits digits (because P must be less or equal to the message that is to be sent) and
+            // starts and ends with a 1, make the flag false to break the loop and save the value of P.
             if (isBinary(input)) {
                 if (input.length() > 1 && input.length() < 32 - numberOfBits && input.charAt(0) == '1' && input.charAt(input.length() - 1) == '1') {
                     flag = false;
                     P.setBinaryValue(input);
                 }
             }
+            // If the flag is still true that means the given input was not valid and a message requesting input must be shown again.
             if (flag)
                 System.out.println("Please give a valid binary number to divide the messages with (P) that starts and ends with 1 and has between 2 and " + (31 - numberOfBits) + " digits:");
         }
 
+        // Default error rate is 0.
         double errorRate = 0;
         System.out.println("\nGive bit error rate (BER) as a decimal:");
+        // Reset the flag to true.
         flag = true;
+        // While loop that stops when the input is valid by making the flag false.
         while (flag) {
             input = in.nextLine();
+            // If the input is a decimal percentage make the flag false to break the loop and save the value of the error rate.
             if (isDecimalPercentage(input)) {
                     flag = false;
                     errorRate = Double.parseDouble(input);
             }
+            // If the flag is still true that means the given input was not valid and a message requesting input must be shown again.
             if (flag)
                 System.out.println("Please give a valid bit error rate (BER) as a decimal that is between 0 and 1:");
         }
 
+        // Default sample count is 1.
         int sampleCount = 1;
         System.out.println("\nGive amount of samples to use:");
+        // Reset the flag to true.
         flag = true;
+        // While loop that stops when the input is valid by making the flag false.
         while (flag) {
             input = in.nextLine();
+            // If the input is an integer and has less than 10 digits, make the flag false to break the loop and save the value of the sample count.
             if (isInteger(input) && input.length() < 10) {
                 flag = false;
                 sampleCount = Integer.parseInt(input);
             }
+            // If the flag is still true that means the given input was not valid and a message requesting input must be shown again.
             if (flag)
                 System.out.println("Please give a valid amount of samples to use:");
         }
 
+        // Default state is to now show messages.
         boolean showMessages = false;
         System.out.println("\nDo you want individual messages to be shown? Enter 1 for yes or 0 for no:");
+        // Reset the flag to true.
         flag = true;
+        // While loop that stops when the input is valid by making the flag false.
         while (flag) {
             input = in.nextLine();
+            // If the input is either 0 or 1, make the flag false to break the loop and save the value of the showMessages variable.
             if (input.equals("1")) {
                 flag = false;
                 showMessages = true;
@@ -78,21 +107,33 @@ public class Main {
                 flag = false;
                 showMessages = false;
             }
+            // If the flag is still true that means the given input was not valid and a message requesting input must be shown again.
             if (flag)
                 System.out.println("Please enter 1 for yes or 0 for no:");
         }
 
+        // The total number of bits of messages that will be transmitted.
         int totalBitCount = numberOfBits + P.toString().length() - 1;
+        // Counter for the amount of messages that changed through transmission.
         int changedCount = 0;
+        // Counter for the amount of messages that changed through transmission and were recognized by the algorithm.
         int caughtChangedCount = 0;
+        // Counter for the amount of messages that changed through transmission and were not recognized by the algorithm.
         int notCaughtChangedCount = 0;
 
+        // For loop that simulates the transmission of sampleCount number of messages.
         for (int i = 0 ; i < sampleCount ; i++) {
+            // The original message without the FCS initialized to 0.
             BinaryNumber originalMessage = new BinaryNumber(0);
+            // The message before being transmitted (with the FCS) initialized to 0.
             BinaryNumber beforeSentMessage = new BinaryNumber(0);
 
+            // For loop that is executed numberOfBits times and where the index counts downwards.
             for (int j = numberOfBits - 1 ; j >= 0 ; j--) {
-                if (Math.random() < 0.49)
+                // If Math.random returns a number that is less than 0.5 (it returns a number such that 0.0 <= Math.random() < 1.0, so Math.random() < 0.5 gives a 50% chance),
+                // then set the j digit of the originalMessage to 1 by doing originalMessage = originalMessage + 2 ^ j (using decimal values). Else if Math.random returns a
+                // number that is more or equal to 0.5, do nothing in which case the loop moves to the next bit since j is decreased by the for loop.
+                if (Math.random() < 0.5)
                     originalMessage.setDecimalValue(originalMessage.getDecimalValue() + (int) Math.pow(2, j));
             }
 

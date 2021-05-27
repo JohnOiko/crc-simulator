1202f234429d5701277f1c46f9c4d5a9bccecaae
John Oiko <ioannisoa@csd.auth.gr>
2021-05-27 15:57:41 +0300

    Added comments to the second half of the class BinaryNumber.java.

--- src/BinaryNumber.java
+++ src/BinaryNumber.java
@@ -49,11 +49,22 @@ public class BinaryNumber {
         return leadingZeroes.concat(binaryString);
     }
 
+    // Method that returns the result of dividend XOR divisor as a BinaryNumber object.
     public static BinaryNumber xorDivide(String dividend, String divisor) {
+        // While the dividend has at least as many digits as the divisor and is not 0 keep dividing.
         while (dividend.length() >= divisor.length() && !(dividend.equals("0"))) {
+            // Make a StringBuilder object of the dividend so as to be able to edit it easier (this is the object
+            // I refer to as the dividend below).
             StringBuilder temp = new StringBuilder(dividend);
+            // Counter for the continuous zeros at the start of the dividend (after applying the xor operator).
             int counter = 0;
+            // Flag to stop updating the counter when a non zero is found in the dividend (after applying the xor operator).
             boolean flag = true;
+            // Compare the digits of the divisor to the digits of the dividend starting from the dividend's most significant bit
+            // and if the digits are the same, change the dividend's bit to 0 (how xor works) and if the flag is true (there are
+            // still continuous zeros in the left part of the dividend) increase the counter. Else if the digits are different
+            // set the dividend's digit to 1 and set the flag to false to signal a 1 has been found (thus the zeros on the left
+            // side of the dividend are no longer continuous).
             for (int i = 0 ; i < divisor.length() ; i++) {
                 if ((temp.charAt(i) == divisor.charAt(i))) {
                     temp.setCharAt(i, '0');
@@ -65,14 +76,20 @@ public class BinaryNumber {
                     flag = false;
                 }
             }
+            // Delete counter number of zeros from the left side of the dividend and update the dividend to the value of the
+            // StringBuilder object temp as a String.
             temp.delete(0, counter);
             dividend = temp.toString();
         }
+        // Once the while loop has stopped, dividend has the result xor calculation and a BinaryNumber is returned with the
+        // value of the dividend.
         return new BinaryNumber(dividend);
     }
 
-        public static int toDecimal(String binaryString) {
+    // Static method that returns the decimal value that corresponds to the given binary number as a string.
+    public static int toDecimal(String binaryString) {
         int decimalValue = 0;
+        // For loop that calculates the decimal value that corresponds to the binary number binaryString.
         for (int i = 0 ; i < binaryString.length() ; i++) {
             decimalValue += Integer.parseInt(binaryString.substring(i, i+1)) * Math.pow(2, binaryString.length() - i - 1);
         }
